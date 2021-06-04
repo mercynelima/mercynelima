7095f958718771c86ccc9629a24fd392c806ba6e
mercynelima <85347024+mercynelima@users.noreply.github.com>
2021-06-04 05:57:27 -0700

    Add files via upload

---
+++ mercyne.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int height,base;
+float ans;/*ans may come in fractions*/
+
+cout<<"enter height and base";
+cin>>height>>base;
+ans=(1/2)*height*base;
+/*mathematical formula*/
+
+cout<<"area if triangle is"<<ans;
+}
